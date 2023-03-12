6301d8fb0673d4b7776d0d78c80cd27821d8f8c5
Banee Ishaque K <Baneeishaque@gmail.com>
2023-03-12 21:36:50 +0530

    Misc

--- Edge_Automation_for_Microsoft_Rewards/Program.cs
+++ Edge_Automation_for_Microsoft_Rewards/Program.cs
@@ -1,4 +1,29 @@
-﻿using OpenQA.Selenium.Edge;
+﻿//using OpenQA.Selenium;
+
+using OpenQA.Selenium.Edge;
+
+//using WebDriverManager;
+//using WebDriverManager.DriverConfigs.Impl;
+
+//var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+//Dictionary<String, String> edgeBrowsers = new() {
+//    { "113.0.1732.0", Path.Combine(localAppData, @"Microsoft\Edge SxS\Application\msedge.exe") },
+//    { "110.0.1587.69", @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" }
+//};
+
+//foreach (var edgeBrowser in edgeBrowsers) {
+
+//    new DriverManager().SetUpDriver(new EdgeConfig(), edgeBrowser.Key);
+
+//    var edgeDriverOptions = new EdgeOptions {
+//        BinaryLocation = edgeBrowser.Value,
+//    };
+
+//    edgeDriverOptions.AddArgument($"--profile-directory=Default");
+
+//    var driver = new EdgeDriver(edgeDriverOptions);
+//}
 
 var driver = new EdgeDriver();
 
@@ -32,3 +57,36 @@ finally {
 
     driver.Quit();
 }
+
+//var element = driver.FindElement(By.Id("sb_form_q"));
+//element.SendKeys("Rashmika Mandanna");
+//element.Submit();
+
+//element = driver.FindElement(By.LinkText("Movies"));
+//element.Click();
+
+//element = driver.FindElement(By.ClassName("items"));
+
+//element = driver.FindElement(By.CssSelector("a[title='Navigate right']"));
+//element.Click();
+
+//var moviesElements = element.FindElements(By.CssSelector("li[class='item col']"));
+
+//string[] movieLinks = Array.Empty<string>();
+
+//foreach (var movieElement in moviesElements) {
+
+//    Console.WriteLine(movieElement.Text);
+
+//    var movieLink = movieElement.FindElement(By.ClassName("cardToggle"));
+//    Console.WriteLine(movieLink.Text);
+
+//    _ = movieLinks.Append(movieLink.GetAttribute("href"));
+//}
+
+//foreach (var movieLink in movieLinks) {
+
+//    driver.Url = movieLink;
+
+//    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(5000);
+//}
