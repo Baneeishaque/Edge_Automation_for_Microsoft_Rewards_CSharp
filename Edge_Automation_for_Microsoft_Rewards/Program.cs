5612af2fca56477fba8b6ba015ef258d27327161
Banee Ishaque K <baneeishaque@gmail.com>
2023-03-12 21:32:22 +0530

    In

---
+++ Edge_Automation_for_Microsoft_Rewards/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium.Edge;
+
+var driver = new EdgeDriver();
+
+try {
+    var i = 0;
+    foreach (var line in File.ReadLines(path: "bing urls.txt")) {
+
+        //Console.WriteLine(line);
+
+        if (i==2) {
+            break;
+        }
+
+        if (!String.IsNullOrWhiteSpace(line)) {
+
+            driver.Url = line;
+
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(15000);
+            if (i==0) {
+
+                Thread.Sleep(7000);
+            }
+            else {
+                Thread.Sleep(2000);
+            }
+        }
+        i++;
+    }
+}
+finally {
+
+    driver.Quit();
+}
